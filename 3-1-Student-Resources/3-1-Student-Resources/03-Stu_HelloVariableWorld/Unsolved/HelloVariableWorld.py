6699fe15df2f21938a1058b710eecd5c3de67f76
msingaram1 <msingaram1@gmail.com>
2021-09-12 21:20:56 -0400

    Adding PyPoll.py file.

---
+++ 3-1-Student-Resources/3-1-Student-Resources/03-Stu_HelloVariableWorld/Unsolved/HelloVariableWorld.py
@@ -0,0 +1,34 @@
+# Create a variable called 'name' that holds a string
+name = "Muru"
+
+# Create a variable called 'country' that holds a string
+
+country = "US"
+# Create a variable called 'age' that holds an integer
+age = 23
+
+# Create a variable called 'hourly_wage' that holds an integer
+
+hourly_wage = 20
+# Calculate the daily wage for the user
+
+daily_wage = hourly_wage * 8 
+# Create a variable called 'satisfied' that holds a boolean
+satisfied = "no"
+
+# Print out "Hello <name>!"
+
+
+# Print out what country the user entered
+# Print out the user's age
+
+print(name + " is from the " + country + ", is " + str(age) + ", and gets paid " + str(hourly_wage) + " dollars an hour")
+
+
+
+# With an f-string, print out the daily wage that was calculated
+# With an f-string, print out whether the users were satisfied
+
+print(f"Daily wage is {daily_wage} and satisfication level (Y/N): {satisfied}")
+
+
---
+++ 3-1-Student-Resources/3-1-Student-Resources/05-Stu_GroceryList/Unsolved/grocery_list.py
@@ -0,0 +1,13 @@
+# Create a Python list to store your grocery list
+list = ["milk", "bread", "eggs", "peanut butter", "jelly"]
+# Print the grocery list
+
+print(list)
+# Change "Peanut Butter" to "Almond Butter" and print out the updated list
+list[3]="almond butter"
+# Remove "Jelly" from grocery list and print out the updated list
+list.remove("jelly")
+print(list)
+# Add "Coffee" to grocery list and print the updated list
+list.append("coffee")
+print(list)
